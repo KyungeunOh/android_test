5365cb667dd1b646eb9454fbf1539e3f18e1801f
Kyung eun <oooo8962@naver.com>
2021-11-24 06:57:43 +0900

    recycler view test

--- app/src/main/java/com/opensource/firebasetest/CustomAdapter.java
+++ app/src/main/java/com/opensource/firebasetest/CustomAdapter.java
@@ -1,5 +1,8 @@
 package com.opensource.firebasetest;
 
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
 import android.widget.TextView;
@@ -7,35 +10,59 @@ import android.widget.TextView;
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
-import com.google.firebase.database.core.view.View;
+import com.bumptech.glide.Glide;
 
-public class CustomAdapter extends RecyclerView.Adapter<CustomAdapter.CustomAdapter> {
-    private
-    ImageView iv_img;
-    TextView tv_name;
-    TextView tv_addr;
-    TextView tv_number;
+import java.util.ArrayList;
 
-    public CustomViewHolder(@NonNull View itemView) {
-        super(itemView);
-        this.iv_img = itemView.findViewById(R.id.iv_img);
-        this.tv_name = itemView.findViewById(R.id.tv_name);
-        this.tv_addr = itemView.findViewById(R.id.tv_addr);
-        this.tv_number = itemView.findViewById(R.id.tv_number);
+public class CustomAdapter extends RecyclerView.Adapter<CustomAdapter.CustomViewHolder> {
+
+    private ArrayList<EcoPlace> arrayList;
+    private Context context;
+    //어댑터에서 액티비티 액션을 가져올 때 context가 필요한데 어댑터에는 context가 없다.
+    //선택한 액티비티에 대한 context를 가져올 때 필요하다.
+
+    public CustomAdapter(ArrayList<EcoPlace> arrayList, Context context) {
+        this.arrayList = arrayList;
+        this.context = context;
     }
+
     @NonNull
     @Override
-    public CustomAdapter.CustomAdapter onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-        return null;
+    //실제 리스트뷰가 어댑터에 연결된 다음에 뷰 홀더를 최초로 만들어낸다.
+    public CustomViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.ecoplace, parent, false);
+        CustomViewHolder holder = new CustomViewHolder(view);
+        return holder;
     }
 
     @Override
-    public void onBindViewHolder(@NonNull CustomAdapter.CustomAdapter holder, int position) {
-
+    public void onBindViewHolder(@NonNull CustomViewHolder holder, int position) {
+        Glide.with(holder.itemView)
+                .load(arrayList.get(position).getImg())
+                .into(holder.iv_img);
+        holder.tv_name.setText(arrayList.get(position).getName());
+        holder.tv_addr.setText(arrayList.get(position).getAddr());
+        holder.tv_number.setText(arrayList.get(position).getNumber());
     }
 
     @Override
     public int getItemCount() {
-        return 0;
+        // 삼항 연산자
+        return (arrayList != null ? arrayList.size() : 0);
+    }
+
+    public class CustomViewHolder extends RecyclerView.ViewHolder {
+        ImageView iv_img;
+        TextView tv_name;
+        TextView tv_addr;
+        TextView tv_number;
+
+        public CustomViewHolder(@NonNull View itemView) {
+            super(itemView);
+            this.iv_img = itemView.findViewById(R.id.iv_img);
+            this.tv_name = itemView.findViewById(R.id.tv_name);
+            this.tv_addr = itemView.findViewById(R.id.tv_addr);
+            this.tv_number = itemView.findViewById(R.id.tv_number);
+        }
     }
 }
--- app/src/main/java/com/opensource/firebasetest/MainActivity.java
+++ app/src/main/java/com/opensource/firebasetest/MainActivity.java
@@ -1,154 +1,74 @@
 package com.opensource.firebasetest;
 
-import androidx.annotation.NonNull;
-import androidx.annotation.Nullable;
-import androidx.appcompat.app.AppCompatActivity;
-
-import android.media.Image;
-import android.net.Uri;
 import android.os.Bundle;
-import android.widget.ArrayAdapter;
-import android.widget.ImageView;
-import android.widget.ListView;
-import android.widget.TextView;
-import android.widget.Toast;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
 
-import com.bumptech.glide.Glide;
-import com.google.android.gms.tasks.OnSuccessListener;
-import com.google.firebase.database.ChildEventListener;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
-import com.google.firebase.storage.FirebaseStorage;
-import com.google.firebase.storage.StorageReference;
-
-import org.json.JSONObject;
-import org.w3c.dom.Text;
 
 import java.util.ArrayList;
-import java.util.List;
 
-public class MainActivity extends AppCompatActivity {
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
-    private FirebaseDatabase mDatabase;
-    private DatabaseReference mReference;
-    private ChildEventListener mChild;
+public class MainActivity extends Fragment {
+    private RecyclerView recyclerView;
+    private RecyclerView.Adapter adapter;
+    private RecyclerView.LayoutManager layoutManager;
+    private ArrayList<EcoPlace> arrayList;
+    private FirebaseDatabase database;
+    private DatabaseReference databaseReference;
 
-    private ListView listView;
-    private ArrayAdapter<Object> adapter;
-    List<Object> Array = new ArrayList<Object>();
+    private View view;
 
+    @Nullable
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-
-        // listView = (ListView)findViewById(R.id.listviewmsg);
-        TextView tname = findViewById(R.id.name);
-        TextView taddr = findViewById(R.id.addr);
-        TextView tnumber = findViewById(R.id.number);
-        TextView timg = findViewById(R.id.img);
-        final String[] imagestr = new String[1];
-
-        initDatabase();
-
-        adapter = new ArrayAdapter<Object>(this, android.R.layout.simple_dropdown_item_1line, new ArrayList<Object>());
-        // listView.setAdapter(adapter);
-
-        mReference = mDatabase.getReference("hotel");
-        mReference.addValueEventListener(new ValueEventListener() {
-            @Override
-            public void onDataChange(@NonNull DataSnapshot snapshot) {
-                adapter.clear();
-
-                for (DataSnapshot hotelData : snapshot.getChildren()){
-                    // Object obj = hotelData.getValue();
-                    for(DataSnapshot details : hotelData.getChildren()){
-                        Object obj = details.getValue();
-                        if(details.getKey().equals("img")){
-                            imagestr[0] = details.getValue().toString();
-                        }
-                        Array.add(obj);
-                        adapter.add(obj);
-                    }
-                    // Array.add(obj);
-                    // adapter.add(obj);
-                }
-
-                timg.setText(imagestr[0]);
-
-
-                adapter.notifyDataSetChanged();
-                // listView.setSelection(adapter.getCount() - 1);
-            }
-
-            @Override
-            public void onCancelled(@NonNull DatabaseError error) {
-
-            }
-        });
-
-        ImageView img_test = findViewById(R.id.img_test);
-
-        FirebaseStorage storage = FirebaseStorage.getInstance("gs://greener-test-c9229.appspot.com/");
-        // StorageReference storageRef = storage.getReference().child(imagestr[0]);
-        StorageReference storageRef = storage.getReference("hotel/seamark.jpg");
-        Glide.with(this).load(storageRef).into(img_test);
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        view = inflater.inflate(R.layout.activity_main, container, false);
+        recyclerView = (RecyclerView) view.findViewById(R.id.recyclerView);
+        recyclerView.setHasFixedSize(true); // 리사이클러뷰 기존성능 강화
 
         /*
-        storageRef.getDownloadUrl().addOnSuccessListener(new OnSuccessListener<Uri>() {
+        layoutManager = new LinearLayoutManager(getContext());
+        recyclerView.setLayoutManager(layoutManager);
+        arrayList = new ArrayList<>(); // User 객체를 담을 어레이 리스트 (어댑터쪽으로)
+
+        database = FirebaseDatabase.getInstance(); // 파이어베이스 데이터베이스 연동
+        databaseReference = database.getReference("cafeList_ Gongneung"); // DB 테이블 연결
+        databaseReference.addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                // 파이어베이스 데이터베이스의 데이터를 받아오는 곳
+                arrayList.clear(); // 기존 배열리스트가 존재하지않게 초기화
+                for (DataSnapshot snapshot : dataSnapshot.getChildren()) { // 반복문으로 데이터 List를 추출해냄
+                    EcoPlace ecoPlace = snapshot.getValue(EcoPlace.class); // 만들어뒀던 User 객체에 데이터를 담는다.
+                    arrayList.add(ecoPlace); // 담은 데이터들을 배열리스트에 넣고 리사이클러뷰로 보낼 준비
+                }
+                adapter.notifyDataSetChanged(); // 리스트 저장 및 새로고침해야 반영이 됨
+            }
 
             @Override
-            public void onSuccess(Uri uri) {
-                Glide.with(getApplicationContext()).load(uri).error(R.drawable.ic_launcher_foreground).into(img_test);
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+                // 디비를 가져오던중 에러 발생 시
+                Log.e("activity_main", String.valueOf(databaseError.toException())); // 에러문 출력
             }
         });
+        adapter = new CustomAdapter(arrayList, getContext());
+        recyclerView.setAdapter(adapter); // 리사이클러뷰에 어댑터 연결
+
+        return view;
+
          */
-        // Glide.with(this).load(storageRef).into(img_test);
-        // Glide.with(MainActivity.this).load(R.drawable.ic_launcher_foreground).into(img_test);
-    }
-
-    private void initDatabase() {
-        mDatabase = FirebaseDatabase.getInstance();
-
-        mReference = mDatabase.getReference("log");
-        mReference.child("log").setValue("check");
-
-        mChild = new ChildEventListener() {
-            @Override
-            public void onChildAdded(@NonNull DataSnapshot snapshot, @Nullable String previousChildName) {
-
-            }
-
-            @Override
-            public void onChildChanged(@NonNull DataSnapshot snapshot, @Nullable String previousChildName) {
-
-            }
-
-            @Override
-            public void onChildRemoved(@NonNull DataSnapshot snapshot) {
-
-            }
-
-            @Override
-            public void onChildMoved(@NonNull DataSnapshot snapshot, @Nullable String previousChildName) {
-
-            }
-
-            @Override
-            public void onCancelled(@NonNull DatabaseError error) {
-
-            }
-        };
-        mReference.addChildEventListener(mChild);
-    }
-
-    @Override
-    protected void onDestroy() {
-        super.onDestroy();
-        mReference.removeEventListener(mChild);
+        return view;
     }
 }
-
